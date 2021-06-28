abec5d6ce711c9d50feaf883e424edd0bee23349
ilhem <ilhem@BRAIN>
2021-06-28 18:32:54 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // upload the xml file
+            string xmlPath = null;
+            if (args != null && args.Length == 1)
+            {
+                xmlPath = args[0];
+                if (xmlPath != null && xmlPath.Length > 0)
+                {
+                    // show the help commande
+                    if (xmlPath.Equals("help"))
+                    {
+                        System.Console.WriteLine("Commande example:  ");
+                        System.Console.WriteLine("DSLQuery.exe C:/app/sample.xml");
+                    }
+                    else
+                    {
+                        Query query = null;
+                        try
+                        {
+                            // load xml query file to an object
+                            query = loadQuery(xmlPath);
+                        }
+                        catch (Exception e)
+                        {
+                            // exception xml error
+                            Console.WriteLine("XML Validation Error: " + e.Message);
+                        }
+                        try
+                        {
+                            if (query != null)
+                            {
+                                // convert from query object to string
+                                Console.WriteLine(query.toSqlString());
+                            }
+                            else
+                            {
+                                Console.WriteLine("SQL Validation Error: The query is empty");
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("SQL Validation Error: " + e.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("the path is empty or there are many path");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Error: The input arguments must be one");
+            }
+
+
+
+
+        }
+
+        static Query loadQuery(String xmlPath)
+        {
+            Query query = new Query();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlPath);
+            // retrieve the child nodes
+            XmlNodeList childNodes = doc.DocumentElement.ChildNodes;
+            if (childNodes.Count > 0)
+            {
+                var queryFields = new List<string>();
+                var filter = new Filter();
+                // iterate the child nodes 
+                foreach (XmlNode node in childNodes)
+                {
+                    // retrieve the fields
+                    string text = node.Name;
+                    if (text.Equals("fields"))
+                    {
+                        XmlNodeList fieldsNodes = node.ChildNodes;
+                        if (fieldsNodes != null && fieldsNodes.Count > 0)
+                        {
+                            foreach (XmlNode fieldsNode in fieldsNodes)
+                            {
+                                string fieldName = fieldsNode.InnerText;
+                                if (fieldName != null && fieldName.Length > 0)
+                                {
+                                    queryFields.Add(fieldName);
+                                }
+                                else
+                                {
+                                    throw new InvalidOperationException("Field found with empty name");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("No Fields found in xml");
+                        }
+                    }
+                    else if (text.Equals("filters"))
+                    {
+                        // retrieve the filters
+                        XmlNodeList filtersNodes = node.ChildNodes;
+                        if (filtersNodes != null && filtersNodes.Count > 0)
+                        {
+                            XmlNode firstNode = filtersNodes.Item(0);
+                            string nodeName = firstNode.Name;
+                            if (filtersNodes.Count == 1)
+                            {
+
+                                if (nodeName != null && nodeName.Length > 0)
+                                {
+                                    if (nodeName.Equals(QueryConst.and) || nodeName.Equals(QueryConst.or))
+                                    {
+                                        filter = initFilters(node);
+                                    }
+                                    else
+                                    {
+
+                                        throw new InvalidOperationException(" XML tag '" + nodeName + "' is not authorized, only 'and' and 'or' logical operator are acceptable in filters node");
+                                    }
+                                }
+                                else
+                                {
+                                    throw new InvalidOperationException("Empty Filter node in xml");
+                                }
+                            }
+                            else
+                            {
+                                if (nodeName.Equals(QueryConst.field) || nodeName.Equals(QueryConst.value) || nodeName.Equals(QueryConst.predicate))
+                                {
+                                    filter = initFilters(node);
+                                }
+                                else
+                                {
+                                    throw new InvalidOperationException(" It is mandatory to have only the following xml child node under 'filters', this child must be 'and' , 'or', 'field', 'value' or 'predicate' xml tag");
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("No Filters found in xml");
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException(" XML tag '" + text + "' is not authorized");
+                    }
+                }
+                query.Fields = queryFields;
+                query.Filter = filter;
+            }
+            else
+            {
+                throw new InvalidOperationException("Empty Query element in xml");
+            }
+            return query;
+        }
+        // recursive function return filter object from xml node
+        private static Filter initFilters(XmlNode filterNode)
+        {
+            var filter = new Filter();
+            XmlNodeList nodes = filterNode.ChildNodes;
+            if (nodes != null && nodes.Count > 0)
+            {
+                if (nodes.Count == 1)
+                {
+                    XmlNode firstNode = nodes.Item(0);
+                    string nodeName = firstNode.Name;
+                    if (nodeName != null && nodeName.Length > 0)
+                    {
+                        if (nodeName.Equals(QueryConst.and) || nodeName.Equals(QueryConst.or))
+                        {
+                            if (nodeName.Equals(QueryConst.and))
+                            {
+                                filter.LogicalOp = QueryConst.and;
+                            }
+                            else if (nodeName.Equals(QueryConst.or))
+                            {
+                                filter.LogicalOp = QueryConst.or;
+                            }
+
+                            XmlNodeList fNodes = firstNode.ChildNodes;
+                            if (fNodes != null && fNodes.Count == 2)
+                            {
+                                var filter1 = initFilters(fNodes.Item(0));
+                                var filter2 = initFilters(fNodes.Item(1));
+                                filter.Filters1 = filter1;
+                                filter.Filters2 = filter2;
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException("It is mandatory to have two 'filter' nodes after 'and / 'or' logical operator");
+                            }
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException(" XML tag '" + nodeName + "' is not authorized, only 'and' and 'or' logical operator are acceptable in filters node");
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Empty Filter node in xml");
+                    }
+                }
+                else
+                {
+                    var singleFilter = new SingleFilter();
+                    foreach (XmlNode n in nodes)
+                    {
+                        if (n != null && QueryConst.field.Equals(n.Name))
+                        {
+                            singleFilter.field = n.InnerText;
+                        }
+
+                        if (n != null && QueryConst.value.Equals(n.Name))
+                        {
+                            singleFilter.value = n.InnerText;
+                        }
+
+                        if (n != null && QueryConst.predicate.Equals(n.Name))
+                        {
+                            singleFilter.predicate = n.InnerText;
+                        }
+                    }
+                    if (singleFilter.field == null || singleFilter.field.Length == 0)
+                    {
+                        throw new InvalidOperationException("Missing field attribute in 'filter' node");
+                    }
+
+                    if (singleFilter.value == null || singleFilter.value.Length == 0)
+                    {
+                        throw new InvalidOperationException("Missing value attribute in 'filter' node");
+                    }
+
+                    filter.SingleFilter = singleFilter;
+                }
+            }
+            return filter;
+        }
+    }
+}
---
+++ Query.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    public class Query
+    {
+        public List<String> Fields { get; set; }
+        public Filter Filter { get; set; }
+        // function convert query object to string
+        public string toSqlString()
+        {
+            StringBuilder query = new StringBuilder();
+            var testTown = QueryConst.towns;
+            // add SELECT to the query
+            query.Append(QueryConst.select).Append(" ");
+            // check if field is not null
+            if (Fields != null && Fields.Count > 0)
+            {
+                // check if field exist in schema
+                foreach (string fildName in Fields)
+                {
+                    if (!testTown.ContainsKey(fildName))
+                    {
+                        throw new Exception("Field '" + fildName + "' in SELECT Clause do not exist in 'Town' schema");
+                    }
+                }
+                // concatenate the filds
+                query.AppendLine(String.Join(",", Fields));
+            }
+            else
+            {
+                throw new Exception("Missing fields in query");
+            }
+            // add FROM Table to the query
+            query.AppendLine(QueryConst.from);
+            // convert filters to string if the query contains filters 
+            if (Filter != null)
+            {
+                string filterString = Filter.toSqlString();
+                if (filterString != null && filterString.Length > 0)
+                {
+                    // add WHERE to the query
+                    query.Append(QueryConst.where).Append(" ");
+                    // add FILTERS to the query
+                    query.AppendLine(Filter.toSqlString());
+
+                }
+
+            }
+            return query.ToString();
+        }
+    }
+
+    public class Filter
+    {
+        public string LogicalOp { get; set; }
+        public Filter Filters1 { get; set; }
+        public Filter Filters2 { get; set; }
+        public SingleFilter SingleFilter { get; set; }
+        // recursive function to convert the filters object to a string
+        internal string toSqlString()
+        {
+            StringBuilder queryPart = new StringBuilder();
+
+            if (LogicalOp != null && SingleFilter == null)
+            {
+                // compounding filter 
+                string Filter1 = Filters1.toSqlString();
+                string Filter2 = Filters2.toSqlString();
+                queryPart.Append("(").Append(Filter1).Append(" ");
+                queryPart.Append(LogicalOp).Append(" ");
+                queryPart.Append(Filter2).Append(")");
+
+            }
+            else
+            {
+                if (SingleFilter != null)
+                {
+                    // simple filter
+                    string filterString = SingleFilter.toSqlString();
+                    if (filterString != null && filterString.Length > 0)
+                    {
+                        queryPart.Append(SingleFilter.toSqlString());
+                    }
+                }
+            }
+            return queryPart.ToString();
+        }
+    }
+
+    public class SingleFilter
+    {
+        public string field { get; set; }
+        public string value { get; set; }
+        public string predicate { get; set; }
+
+        internal string toSqlString()
+        {
+            StringBuilder queryPart = new StringBuilder();
+            if (predicate == null || predicate.Length == 0)
+            {
+                predicate = QueryConst.predicateEq;
+            }
+            // validation of SQL Query
+            validation();
+            // concate the filters
+            queryPart.Append(field).Append(" ");
+            queryPart.Append(convertPredicate(predicate)).Append(" ");
+            if (QueryConst.predicateContains.Equals(predicate))
+            {
+                queryPart.Append("'%").Append(value).Append("%'");
+            }
+            else
+            {
+                queryPart.Append(value).Append(" ");
+            }
+            return queryPart.ToString();
+        }
+        void validation()
+        {
+            var testTown = QueryConst.towns;
+            // check if the type of field is integer or float and the value is int or float  when the predicate is '<', '>' or '='
+            if (QueryConst.predicateEq.Equals(predicate) || QueryConst.predicateGt.Equals(predicate) || QueryConst.predicateLt.Equals(predicate))
+            {
+                // process the validation of the part where the field is average_age by itself
+                if (!field.Equals(QueryConst.average_age))
+                {
+                    // exception when the filed type is not integer
+                    if (!testTown[field].Equals(QueryConst.integer))
+                    {
+                        throw new Exception("Invalide operation: the predicate '" + convertPredicate(predicate) + "' is not valide with varchar type, the field must be a number. the type of '" + field + "' is '" + testTown[field] + "' ");
+                    }
+                    else
+                    {
+                        // exception when the value is not integer
+                        int n;
+                        if (!int.TryParse(value, out n))
+                        {
+                            throw new Exception("Invalide operation: The value of '" + field + "' must be '" + testTown[field] + "' with the predicate '" + convertPredicate(predicate) + "' ");
+                        }
+                    }
+                }
+                else
+                {
+                    // exception when the filed type is not float
+                    if (!testTown[field].Equals(QueryConst.flottant))
+                    {
+                        throw new Exception("Invalide operation: the predicate '" + convertPredicate(predicate) + "' is not valide with varchar type, the field must be a number. the type of '" + field + "' is '" + testTown[field] + "' ");
+                    }
+                    else
+                    {
+                        // exception when the value is not float
+                        float n;
+                        if (!Single.TryParse(value, out n))
+                        {
+                            throw new Exception("Invalide operation: The value of '" + field + "' must be a '" + testTown[field] + "' with the predicate '" + convertPredicate(predicate) + "' ");
+                        }
+
+                    }
+                }
+            }
+            // check if the type of field is varchar and the value is string when the predicate is LIKE
+            if (QueryConst.predicateContains.Equals(predicate))
+            {
+                // exception when the filed type is not varchar
+                if (!testTown[field].Equals(QueryConst.varchar))
+                {
+                    throw new Exception("Invalide operation: the predicate '" + convertPredicate(predicate) + "' is not valide with integer type, the field must be varchar. the type of '" + field + "' is '" + testTown[field] + "' ");
+                }
+                else
+                {
+                    // exception when the value is not varchar
+                    int n;
+                    float number;
+                    if (int.TryParse(value, out n) || Single.TryParse(value, out number))
+                    {
+                        throw new Exception("Invalide operation: The value of '" + field + "' must be '" + testTown[field] + "' with the predicate '" + convertPredicate(predicate) + "' ");
+                    }
+                }
+            }
+        }
+        internal string convertPredicate(String predicate)
+        {
+            if (QueryConst.predicateEq.Equals(predicate))
+            {
+                return QueryConst.sqlEq;
+            }
+
+            if (QueryConst.predicateGt.Equals(predicate))
+            {
+                return QueryConst.sqlGt;
+            }
+
+            if (QueryConst.predicateLt.Equals(predicate))
+            {
+                return QueryConst.sqlLt;
+            }
+
+            if (QueryConst.predicateContains.Equals(predicate))
+            {
+                return QueryConst.sqlLike;
+            }
+
+            return null;
+        }
+    }
+
+    public static class QueryConst
+    {
+
+        public static readonly string and = "and";
+        public static readonly string or = "or";
+        public static readonly string filter = "filter";
+        public static readonly string field = "field";
+        public static readonly string value = "value";
+        public static readonly string predicate = "predicate";
+        public static readonly string fields = "fields";
+        public static readonly string filters = "filters";
+
+        public static readonly string select = "SELECT";
+        public static readonly string from = "FROM towns";
+        public static readonly string where = "WHERE";
+
+        public static readonly string predicateEq = "eq";
+        public static readonly string predicateGt = "gt";
+        public static readonly string predicateLt = "lt";
+        public static readonly string predicateContains = "contains";
+
+        public static readonly string sqlEq = "=";
+        public static readonly string sqlGt = ">";
+        public static readonly string sqlLt = "<";
+        public static readonly string sqlLike = "LIKE";
+
+        public static readonly string integer = "integer";
+        public static readonly string varchar = "varchar";
+        public static readonly string flottant = "float";
+
+        public static readonly string average_age = "average_age";
+
+        public static readonly Dictionary<string, string> towns = new Dictionary<string, string>()
+        {
+            { "id", "integer" },
+            { "code", "integer" },
+            { "name", "varchar" },
+            { "population", "integer" },
+            { "average_age", "float" },
+            { "district_code", "integer" },
+            { "department_code", "varchar" },
+            { "region_code", "integer" },
+            { "region_name", "varchar" },
+
+        };
+    }
+}
